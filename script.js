2e542ed98bcbf3aebcc7c496d577a91756c2e202
Gleibsonfernandes <gleibsonfernandes17@gmail.com>
2023-03-13 20:12:49 -0300

    notificação
    Notificação e busca concluido

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function borda(){
+    let procura = window.document.getElementById("procura-aside-cont")
+    
+    procura.style.border="solid 1px black"
+
+    
+
+}
+
+function outBorder(){
+    
+    let procura = window.document.getElementById("procura-aside-cont")
+    procura.style.border="none"
+}
